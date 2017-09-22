da9a05eebaaf18d5efe929936eb39a79c10d38f3
queenskid <andres.ibarra0716@gmail.com>
2017-09-22 14:05:00 -0700

    problem2 solved

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.
 
-For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
+For DONE item, be sure to change it to say DONE when you have successfully completed the task*/
 
 function sum(a,b){
   var theSum = a + b;
@@ -28,12 +28,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // TODO: Write your code here
-function multiply(a,b){ //eslint-disable-line
-
+function multiply(a, b){
+  var theProduct = (a * b);
+  console.log('product', theProduct);
+  var message = ('The product of ' + a + ' and ' + b + ' is ' + theProduct + '.');
+  return[theProduct, message];
 }
+testMultiply(5,9);
 
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
